e70db656f075d57511c2b0ce80f3652574320070
yeasinarafat51 <aloondokar65@gmail.com>
2024-10-22 15:15:20 +0600

    counter

--- src/components/redux/Store.js
+++ src/components/redux/Store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from '../features/counter/counnterSlice'
 
 export const store = configureStore({
-    reducer:{}
-})
+    reducer: {
+      counter: counterReducer,
+    },
+  })
