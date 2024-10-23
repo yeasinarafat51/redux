d7e54dedfcdbfbfc13bc6a5e07243efebac77031
yeasinarafat51 <aloondokar65@gmail.com>
2024-10-24 02:17:36 +0600

    loop

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-const order = 1;
+// const order = 1;
 
-const first = order.toString().padStart(4, "0");
+// const first = order.toString().padStart(4, "0");
 
-const second = parseInt(first.substring(3)) + 1;
+// const second = parseInt(first.substring(3)) + 1;
 
-const secid = second.toString().padStart(4, "0");
-console.log(secid)
+// const secid = second.toString().padStart(4, "0");
+// console.log(secid)
+
+
+let mangopot = 0;
+for (let mango = 0; mango < 10 ; mango ++){
+    mangopot = mangopot + 1
+}
+console.log(mangopot)
