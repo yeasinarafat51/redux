636c097b27618f1323f0480251dc48c79dd44a73
yeasinarafat51 <aloondokar65@gmail.com>
2024-10-25 01:14:10 +0600

    forin

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 // const order = 1;
 
 // const first = order.toString().padStart(4, "0");
@@ -12,4 +13,20 @@ let mangopot = 0;
 for (let mango = 0; mango < 10 ; mango ++){
     mangopot = mangopot + 1
 }
-console.log(mangopot)
+// console.log(mangopot)
+
+// const names =[ 'mir', 'yeasin', 'dhaka']
+
+// for(let name of names){
+//     console.log('name', name)
+// }
+
+const man = {
+    age: 25,
+    name:'yeasin',
+    pro: true,
+};
+for(let key in man){
+    console.log(man[key])
+}
+
