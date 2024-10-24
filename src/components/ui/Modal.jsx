88b00816bf473fd5e97a084397af5465afe18e9f
yeasinarafat51 <aloondokar65@gmail.com>
2024-10-25 03:15:48 +0600

    from

--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -19,7 +19,7 @@ export default function Modal({isOpen, setIsOpen,title,children}) {
           <div className="flex min-h-full items-center justify-center p-4">
             <DialogPanel
               transition
-              className="w-full max-w-md rounded-xl bg-black p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
+              className="w-full max-w-md rounded-xl bg-green-900 p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
             >
               <DialogTitle as="h3" className="text-base/7 font-medium text-white">
               {title}
