762e6a99dd86123e89749f964d3d8f530e2738fc
Yeasin Arafat <108508524+yeasinarafat51@users.noreply.github.com>
2024-12-17 18:55:53 +0600

    Update Rutin.js

--- src/Rutin.js
+++ src/Rutin.js
@@ -1,3 +1,3 @@
 microservice sikte hohe, uber clone korte hobe
 ajk kisu hoi nai ajk o kisu hoi nai,ajk o kisu hoi nai
-ajk kew o kisu hoi nai,ajk k o
+ajk kew o kisu hoi nai,ajk k o,kisu kora hoi na
