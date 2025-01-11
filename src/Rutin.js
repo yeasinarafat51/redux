ca56ca36c558b4ff1f90ce074cda453aeb5c3e9b
Yeasin Arafat <108508524+yeasinarafat51@users.noreply.github.com>
2025-01-11 19:52:13 +0600

    Update Rutin.js

--- src/Rutin.js
+++ src/Rutin.js
@@ -6,4 +6,4 @@ ajl o kisu hoi nai,ajk o kori naionek din kisu kori na
 sjjdsoaososl,ajk aktu korce
 kono kaj hoi naikkora hoi ns,ajk ke o hoi nai,kisu kori nai
 kaj kora hoi nai
-ajk hoi nai kisu hoi nai
+ajk hoi nai kisu hoi nai,ajk o kisu kori nai
